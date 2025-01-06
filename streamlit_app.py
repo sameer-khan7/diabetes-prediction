fb80650b69d012af5f2204bddff431ef553b1b12
Sameer Khan <57252636+sameer-khan7@users.noreply.github.com>
2025-01-06 17:59:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,29 +2,18 @@ import streamlit as st
 import login
 import prediction
 import dashboard
+import user_management
 
 # Initialize session state for navigation
 if "page" not in st.session_state:
     st.session_state.page = "login"
-    
-
-# Top navigation bar
-#def show_top_bar():
-#    st.sidebar.markdown("---")
-#    st.sidebar.button("Log Out", on_click=logout)
-
-# Logout functionality
-#def logout():
-#    st.session_state.page = "login"
-#    st.session_state.pop("username", None)  # Clear logged-in user data
-#    st.rerun()
 
 # Navigation logic
 if st.session_state.page == "login":
     login.login_page()
 elif st.session_state.page == "prediction":
-#    show_top_bar()  # Show logout button in the sidebar
     prediction.prediction_page()
+elif st.session_state.page == "profile":
+    user_management.profile_page()
 elif st.session_state.page == "dashboard":
-#    show_top_bar()  # Show logout button in the sidebar
     dashboard.dashboard_page()
